6eef40415e94bc7745005fc6ff4d9cbe0e04e225
Vincent Pasquier <kreamax@gmail.com>
2014-06-03 16:19:39 +0200

    création de get_page

--- crawler.py
+++ crawler.py
@@ -1,4 +1,5 @@
 import urllib.request
+import index
 
 from index import *
 
@@ -6,3 +7,30 @@ class Crawler(object):
 
     def __init__(self, index):
         self.index = index
+
+    @staticmethod
+    def get_all_links(page):
+    	pass
+
+    @staticmethod
+    def get_next_target(page, start = 0):
+    	pass
+
+    @staticmethod
+    def get_page(url):
+    	'''
+    	Récupère le contenu d'une page HTML située à une url donnée.
+    	Retourn une chaîne de caractère, vide en cas d'erreur.
+
+    	'''
+
+    	try:
+    		html = urllib.request.urlopen(url).read()
+    		txt = str(html)
+
+    	else:
+    		txt = ''
+
+    	return txt
+    	
+
